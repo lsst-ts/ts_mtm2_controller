69cf4549471c9be4644f13206e053de13aea214e
Te-Wei Tsai <twtsaia@gmail.com>
2026-06-23 12:50:41 -0700

    Add new ILC commands to InnerLoopController.

--- src/daq/inner_loop_controller.rs
+++ src/daq/inner_loop_controller.rs
@@ -23,9 +23,13 @@ use crc::{Crc, CRC_16_MODBUS};
 
 use crate::constants::{
     BROADCAST_ADDRESS, CODE_FORCE_REQUEST, CODE_ILC_MODE, CODE_MONITOR_SENSOR,
-    CODE_STEP_MOTOR_BROADCAST, NUM_ACTUATOR, NUM_ILC_TEMPERATURE_MONITOR_SENSOR,
+    CODE_READ_CALIBRATION_DATA, CODE_REPORT_SERVER_ID, CODE_REPORT_SERVER_STATUS, CODE_RESET,
+    CODE_SCAN_RATE, CODE_SET_OFFSET_AND_SENSITIVITY, CODE_STEP_MOTOR_BROADCAST, NUM_ACTUATOR,
+    NUM_ILC_TEMPERATURE_MONITOR_SENSOR,
 };
+use crate::daq::calibration_data::CalibrationData;
 use crate::enums::InnerLoopControlMode;
+use crate::utility::get_f32_values_from_u8_array;
 
 pub struct InnerLoopController {
     // Cyclic redundancy check (CRC) calculator for the inner-loop controller
@@ -51,8 +55,9 @@ impl Default for InnerLoopController {
 
 impl InnerLoopController {
     /// Create a new inner-loop controller (ILC) by following the document:
-    /// LSST-ILC Firmware: MODBUS Protocol Interface Control Document for M2
-    /// Support System
+    /// 1. LSST-ILC Firmware: MODBUS Protocol Interface Control Document for M2
+    ///    Support System
+    /// 2. LTS-346, ILC Communications Protocol For M2 Support System
     ///
     /// # Returns
     /// A new ILC instance.
@@ -334,6 +339,151 @@ impl InnerLoopController {
         self.communication_counter
     }
 
+    /// Create a frame to report the server ID that contains the server
+    /// identifier information.
+    ///
+    /// # Arguments
+    /// * `address` - The 0-based address of the inner-loop controller (ILC) to
+    ///   report the server ID.
+    ///
+    /// # Returns
+    /// A byte array representing the frame to report the server ID.
+    pub fn create_frame_report_server_id(&self, address: u8) -> [u8; 4] {
+        self.create_frame_code_only(address, CODE_REPORT_SERVER_ID)
+    }
+
+    /// Create a frame with only the address and code for the inner-loop
+    /// controller (ILC).
+    ///
+    /// # Arguments
+    /// * `address` - The 0-based address of ILC.
+    /// * `code` - The code representing the specific command or request.
+    ///
+    /// # Returns
+    /// A byte array representing the frame with only the 1-based address,
+    /// code, and cyclic redundancy check (CRC).
+    fn create_frame_code_only(&self, address: u8, code: u8) -> [u8; 4] {
+        let mut frame = [0; 4];
+        frame[0] = Self::to_one_based_address(address);
+        frame[1] = code;
+
+        Self::calculate_crc_and_update_frame(&self._crc, &mut frame);
+
+        frame
+    }
+
+    /// Create a frame to report the server status that reports the mode,
+    /// status, and faults.
+    ///
+    /// # Arguments
+    /// * `address` - The 0-based address of the inner-loop controller (ILC) to
+    ///   report the server status.
+    ///
+    /// # Returns
+    /// A byte array representing the frame to report the server status.
+    pub fn create_frame_report_server_status(&self, address: u8) -> [u8; 4] {
+        self.create_frame_code_only(address, CODE_REPORT_SERVER_STATUS)
+    }
+
+    /// Create a frame to get the scan rate of the inner-loop controller (ILC).
+    ///
+    /// # Arguments
+    /// * `address` - The 0-based address of ILC.
+    ///
+    /// # Returns
+    /// A byte array representing the frame to get the scan rate.
+    pub fn create_frame_get_scan_rate(&self, address: u8) -> [u8; 5] {
+        // 0xFF = No Change - Respond with the current scan rate
+        self.create_frame_set_scan_rate(address, 0xFF)
+    }
+
+    /// Create a frame to set the scan rate of the inner-loop controller (ILC).
+    ///
+    /// # Arguments
+    /// * `address` - The 0-based address of ILC.
+    /// * `scan_rate` - The scan rate value.
+    ///   0xFF = No Change - Respond with the current scan rate
+    ///   0 = 50 (scan rate in samples per second)
+    ///   1 = 60
+    ///   2 = 100
+    ///   3 = 120
+    ///   4 = 200
+    ///   5 = 240
+    ///   6 = 300
+    ///   7 = 400
+    ///   8 = 480
+    ///   9 = 600
+    ///   10 = 1200
+    ///   11 = 2400
+    ///   12 = 4800
+    ///
+    /// # Returns
+    /// A byte array representing the frame to set the scan rate.
+    pub fn create_frame_set_scan_rate(&self, address: u8, scan_rate: u8) -> [u8; 5] {
+        let mut frame = [0; 5];
+        frame[0] = Self::to_one_based_address(address);
+        frame[1] = CODE_SCAN_RATE;
+        frame[2] = scan_rate;
+
+        Self::calculate_crc_and_update_frame(&self._crc, &mut frame);
+
+        frame
+    }
+
+    /// Create a frame to set the offset and sensitivity for a specific channel.
+    ///
+    /// # Arguments
+    /// * `address` - The 0-based address of the inner-loop controller (ILC).
+    /// * `channel` - The 0-based channel number (0-3) to set the offset and
+    ///   sensitivity.
+    /// * `offset` - The offset value.
+    /// * `sensitivity` - The sensitivity value.
+    ///
+    /// # Returns
+    /// A byte array representing the frame to set the offset and sensitivity.
+    pub fn create_frame_set_offset_and_sensitivity(
+        &self,
+        address: u8,
+        channel: u8,
+        offset: f32,
+        sensitivity: f32,
+    ) -> [u8; 13] {
+        let mut frame = [0; 13];
+        frame[0] = Self::to_one_based_address(address);
+        frame[1] = CODE_SET_OFFSET_AND_SENSITIVITY;
+        frame[2] = Self::to_one_based_address(channel);
+        frame[3..7].copy_from_slice(&offset.to_be_bytes());
+        frame[7..11].copy_from_slice(&sensitivity.to_be_bytes());
+
+        Self::calculate_crc_and_update_frame(&self._crc, &mut frame);
+
+        frame
+    }
+
+    /// Create a frame to reset the inner-loop controller (ILC).
+    ///
+    /// # Arguments
+    /// * `address` - The 0-based address of ILC to reset.
+    ///
+    /// # Returns
+    /// A byte array representing the frame to reset the ILC.
+    pub fn create_frame_reset(&self, address: u8) -> [u8; 4] {
+        self.create_frame_code_only(address, CODE_RESET)
+    }
+
+    /// Create a frame to read calibration data from the inner-loop controller
+    /// (ILC) EEPROM.
+    ///
+    /// # Arguments
+    /// * `address` - The 0-based address of ILC to read the calibration data.
+    ///
+    /// # Returns
+    /// A byte array representing the frame to read calibration data from the
+    /// ILC EEPROM.
+    pub fn create_frame_read_calibration_data(&self, address: u8) -> [u8; 4] {
+        self.create_frame_code_only(address, CODE_READ_CALIBRATION_DATA)
+    }
+
     /// Get the inner-loop control mode from a received frame.
     ///
     /// # Arguments
@@ -361,7 +511,9 @@ impl InnerLoopController {
     /// # Returns
     /// A tuple containing the status, encoder count, and force (in Newtons)
     /// extracted from the frame. If the frame is too short to contain the
-    /// required data, it returns `None`.
+    /// required data, it returns `None`. For the details of status, see
+    /// `Self::check_actuator_ilc_status()` and
+    /// `Self::is_expected_communication_counter()`.
     pub fn get_force_and_status_from_frame(&self, frame: &[u8]) -> Option<(u8, i32, f32)> {
         if frame.len() != 9 {
             return None;
@@ -442,36 +594,7 @@ impl InnerLoopController {
             return None;
         }
 
-        self.get_f32_values_from_u8_array::<4>(frame)
-    }
-
-    /// Get the floating-point values from a byte array. The byte array is
-    /// expected to contain `N` consecutive 4-byte big-endian floating-point
-    /// values.
-    ///
-    /// # Arguments
-    /// * `frame` - The byte array containing the floating-point values.
-    ///
-    /// # Returns
-    /// An array of `N` floating-point numbers extracted from the byte array.
-    /// Returns `None` if the length of the byte array does not match the
-    /// expected length of `N * 4` bytes.
-    fn get_f32_values_from_u8_array<const N: usize>(&self, frame: &[u8]) -> Option<[f32; N]> {
-        if frame.len() != N * 4 {
-            return None;
-        }
-
-        let mut values = [0.0; N];
-        for index in 0..N {
-            values[index] = f32::from_be_bytes([
-                frame[index * 4],
-                frame[index * 4 + 1],
-                frame[index * 4 + 2],
-                frame[index * 4 + 3],
-            ]);
-        }
-
-        Some(values)
+        get_f32_values_from_u8_array::<4>(frame)
     }
 
     /// Get the displacement values from a received frame.
@@ -493,7 +616,7 @@ impl InnerLoopController {
             return None;
         }
 
-        self.get_f32_values_from_u8_array::<12>(frame)
+        get_f32_values_from_u8_array::<12>(frame)
     }
 
     /// Get the inclinometer value from a received frame.
@@ -511,6 +634,94 @@ impl InnerLoopController {
         }
 
         Some(f32::from_be_bytes([frame[0], frame[1], frame[2], frame[3]]))
+    }
+
+    /// Get the server status from a received frame.
+    ///
+    /// # Notes
+    /// Status:
+    /// bit 0: Major Fault - 0 = None, 1 = Major System Fault
+    /// bit 1: Minor Fault - 0 = None, 1 = Minor System Fault
+    /// bit 2: Reserved - 0
+    /// bit 3: Fault Override - 1 = One or more faults are overridden
+    /// bit 4: Cal Main - 1 = Main Calibration Error
+    /// bit 5: Cal Back - 1 = Backup Calibration Error
+    /// bit 6..7: Reserved - 0
+    /// bit 8: Limit Switch 1 - 1 = Limit 1 activated
+    /// bit 9: Limit Switch 2 - 1 = Limit 2 activated
+    /// bit 10..11: Reserved - 0
+    /// bit 12: Monitor Instrument - 1 = Communications timeout with instrument
+    ///                              monitor
+    /// bit 13..15: Reserved - 0
+    ///
+    /// Faults:
+    /// bit 0: Unique ID - 0 = Unique ID verified, 1 = Unique ID CRC error
+    /// bit 1: App Type - 0 = App Type and Network Node Type match,
+    ///                   1 = App Type and Network Node Type do not match
+    /// bit 2: No App - 0 = ILC App present, 1 = No ILC App programmed
+    /// bit 3: App CRC - 0 = ILC-App verified, 1 = ILC-App CRC error
+    /// bit 4: No 1-Wire - 0 = 1-Wire present, 1 = No 1-Wire found
+    /// bit 5: 1-Wire 1 - 0 = 1-Wire copy 1 verified,
+    ///                   1 = 1-Wire copy 1 error
+    /// bit 6: 1-Wire 2 - 0 = 1-Wire copy 2 verified,
+    ///                   1 = 1-Wire copy 2 error
+    /// bit 7: Reserved - 0
+    /// bit 8: Watchdog Reset - 0 = Watchdog timer OK,  
+    ///                         1 = A reset occurred due to Watchdog timeout
+    /// bit 9: Brown-Out - 0 = OK, 1 = Power brown-out occurred
+    /// bit 10: Event Trap - 0 = OK,
+    ///                      1 = A reset occurred due to controller event trap
+    /// bit 11: Motor Driver - 0 = OK, 1 = Motor driver fail
+    /// bit 12: SSR Power - 0 = OK, 1 = SSR power fail
+    /// bit 13: Aux Power - 0 = OK, 1 = Aux power fail
+    /// bit 14: SMC Power - 0 = OK, 1 = Motor controller power fail
+    /// bit 15: Reserved - 0
+    ///
+    /// # Arguments
+    /// * `frame` - The received frame containing the server status data.
+    ///
+    /// # Returns
+    /// An optional tuple containing the inner-loop control mode, status, and
+    /// faults extracted from the frame. Returns `None` if the frame length is
+    /// not exactly 5 bytes.
+    pub fn get_server_status_from_frame(
+        &self,
+        frame: &[u8],
+    ) -> Option<(InnerLoopControlMode, u16, u16)> {
+        if frame.len() != 5 {
+            return None;
+        }
+
+        let mode_value = u16::from_be_bytes([0, frame[0]]);
+        let mode = Self::get_mode_from_value(mode_value);
+
+        let status = u16::from_be_bytes([frame[1], frame[2]]);
+        let faults = u16::from_be_bytes([frame[3], frame[4]]);
+
+        Some((mode, status, faults))
+    }
+
+    /// Get the calibration data from a received frame.
+    ///
+    /// # Arguments
+    /// * `frame` - The received frame containing the calibration data.
+    ///
+    /// # Returns
+    /// An optional tuple containing two `CalibrationData` structs, one for the
+    /// main calibration data and one for the backup calibration data. Returns
+    /// `None` if the frame length is not exactly 96 bytes.
+    pub fn get_calibration_data_from_frame(
+        &self,
+        frame: &[u8],
+    ) -> Option<(CalibrationData, CalibrationData)> {
+        if frame.len() != 96 {
+            return None;
+        }
+
+        Some((
+            CalibrationData::from_frame(&frame[0..48])?,
+            CalibrationData::from_frame(&frame[48..96])?,
+        ))
     }
 
     /// Verify the cyclic redundancy check (CRC) of a received frame.
@@ -779,6 +990,96 @@ mod tests {
     }
 
     #[test]
+    fn test_create_frame_report_server_id() {
+        let ilc = InnerLoopController::new();
+
+        let address = 0x02;
+        let frame = ilc.create_frame_report_server_id(address);
+
+        assert_eq!(frame, [address + 1, CODE_REPORT_SERVER_ID, 0xC1, 0x4C]);
+    }
+
+    #[test]
+    fn test_create_frame_code_only() {
+        let ilc = InnerLoopController::new();
+
+        let address = 0x01;
+        let code = 0x10;
+        let frame = ilc.create_frame_code_only(address, code);
+
+        assert_eq!(frame, [address + 1, code, 0x1, 0x1C]);
+    }
+
+    #[test]
+    fn test_create_frame_report_server_status() {
+        let ilc = InnerLoopController::new();
+
+        let address = 0x03;
+        let frame = ilc.create_frame_report_server_status(address);
+
+        assert_eq!(frame, [address + 1, CODE_REPORT_SERVER_STATUS, 0x83, 0x7D]);
+    }
+
+    #[test]
+    fn test_create_frame_get_scan_rate() {
+        let ilc = InnerLoopController::new();
+
+        let address = 0x01;
+        let frame = ilc.create_frame_get_scan_rate(address);
+
+        assert_eq!(frame, [address + 1, CODE_SCAN_RATE, 0xFF, 0xAC, 0x40]);
+    }
+
+    #[test]
+    fn test_create_frame_set_scan_rate() {
+        let ilc = InnerLoopController::new();
+
+        let address = 0x02;
+        let scan_rate = 0x05;
+        let frame = ilc.create_frame_set_scan_rate(address, scan_rate);
+
+        assert_eq!(frame, [address + 1, CODE_SCAN_RATE, scan_rate, 0x7D, 0xC3]);
+    }
+
+    #[test]
+    fn test_create_frame_set_offset_and_sensitivity() {
+        let ilc = InnerLoopController::new();
+
+        let address = 0x01;
+        let channel = 0x02;
+        let offset = 1.23;
+        let sensitivity = 4.56;
+        let frame =
+            ilc.create_frame_set_offset_and_sensitivity(address, channel, offset, sensitivity);
+
+        assert_eq!(frame[0], address + 1);
+        assert_eq!(frame[1], CODE_SET_OFFSET_AND_SENSITIVITY);
+        assert_eq!(frame[2], channel + 1);
+        assert_eq!(&frame[3..7], &offset.to_be_bytes());
+        assert_eq!(&frame[7..11], &sensitivity.to_be_bytes());
+    }
+
+    #[test]
+    fn test_create_frame_reset() {
+        let ilc = InnerLoopController::new();
+
+        let address = 0x02;
+        let frame = ilc.create_frame_reset(address);
+
+        assert_eq!(frame, [address + 1, CODE_RESET, 0x40, 0xAF]);
+    }
+
+    #[test]
+    fn test_create_frame_read_calibration_data() {
+        let ilc = InnerLoopController::new();
+
+        let address = 0x03;
+        let frame = ilc.create_frame_read_calibration_data(address);
+
+        assert_eq!(frame, [address + 1, CODE_READ_CALIBRATION_DATA, 0x82, 0x9C]);
+    }
+
+    #[test]
     fn test_get_ilc_mode_from_frame() {
         let ilc = InnerLoopController::new();
 
@@ -905,6 +1206,49 @@ mod tests {
         assert_eq!(inclinometer, 10.0);
 
         assert!(ilc.get_inclinometer_from_frame(&[0x41, 0x20]).is_none());
+    }
+
+    #[test]
+    fn test_get_server_status_from_frame() {
+        let ilc = InnerLoopController::new();
+
+        // Valid frame with server status data
+        let frame = [0x01, 0x01, 0x02, 0x03, 0x04];
+
+        let (mode, status, faults) = ilc.get_server_status_from_frame(&frame).unwrap();
+
+        assert_eq!(mode, InnerLoopControlMode::Disabled);
+        assert_eq!(status, 0x0102);
+        assert_eq!(faults, 0x0304);
+
+        // Invalid frame length
+        assert!(ilc.get_server_status_from_frame(&[0x00]).is_none());
+    }
+
+    #[test]
+    fn test_get_calibration_data_from_frame() {
+        let ilc = InnerLoopController::new();
+
+        // Create a valid frame with calibration data
+        let mut frame = [0; 96];
+        for idx in 0..24 {
+            let value = idx as f32;
+            frame[idx * 4..(idx + 1) * 4].copy_from_slice(&value.to_be_bytes());
+        }
+
+        let (main_calibration, backup_calibration) =
+            ilc.get_calibration_data_from_frame(&frame).unwrap();
+
+        assert_eq!(main_calibration.gains, [0.0, 1.0, 2.0, 3.0]);
+        assert_eq!(main_calibration.offsets, [4.0, 5.0, 6.0, 7.0]);
+        assert_eq!(main_calibration.sensitivities, [8.0, 9.0, 10.0, 11.0]);
+
+        assert_eq!(backup_calibration.gains, [12.0, 13.0, 14.0, 15.0]);
+        assert_eq!(backup_calibration.offsets, [16.0, 17.0, 18.0, 19.0]);
+        assert_eq!(backup_calibration.sensitivities, [20.0, 21.0, 22.0, 23.0]);
+
+        // Invalid frame length
+        assert!(ilc.get_calibration_data_from_frame(&[0x00]).is_none());
     }
 
     #[test]
